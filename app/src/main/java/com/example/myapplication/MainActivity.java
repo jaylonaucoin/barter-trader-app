b5dc89912e3a6fd696c509a38b22bf513a1dd0ec
Christopher Hage <hage.chris@yahoo.com>
2023-10-02 01:22:36 -0300

    Revert "Revert "Realtime Firebase added""
    This reverts commit 284f5d0b523fb0c9a1f77b96d9b2efd36f0ff500.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase firebaseDB;
+    private DatabaseReference firebaseDBRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectToFirebase();
+        writeToFirebase();
+    }
+
+    private void connectToFirebase(){
+        firebaseDB = FirebaseDatabase.getInstance("https://barter-app-50729-default-rtdb.firebaseio.com/");
+        firebaseDBRef = firebaseDB.getReference("test");
+    }
+
+    private void writeToFirebase(){
+        firebaseDBRef.setValue("Hello World");
     }
 }
