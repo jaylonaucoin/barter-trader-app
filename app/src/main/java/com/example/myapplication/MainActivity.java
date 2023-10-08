736fa2bda9a1054c87de8188c32d15b187e6b23e
Roshan George Sebastian <rs385930@dal.ca>
2023-10-08 20:42:08 -0300

    Replaced Ratings with a RatingBar and Fixed Bug of Fragments not Displaying

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.myapplication.user_profile_page.UserProfile;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    // temporary change main activity to user profile for testing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, UserProfile.class);
+        startActivity(intent);
     }
 }
