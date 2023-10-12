8292ffe6c436258840feb0426fe6dfe57e2a7581
Christopher Hage <hage.chris@yahoo.com>
2023-10-12 16:54:00 -0300

    Refactored, Firebase, Christopher Hage

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void connectToFirebase(){
-        firebaseDB = FirebaseDatabase.getInstance("https://barter-app-50729-default-rtdb.firebaseio.com/");
+        firebaseDB = FirebaseDatabase.getInstance();
         firebaseDBRef = firebaseDB.getReference("test");
     }
 
