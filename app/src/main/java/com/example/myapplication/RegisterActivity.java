d080e081c2eb16aae3acaf700d4d399c9aaba2eb
jaucoin <jy349203@dal.ca>
2023-10-13 20:47:45 -0300

    Fixed the UID assignment in the DB and implemented an error handler for duplicate email

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -8,23 +8,31 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDB;
     private DatabaseReference firebaseDBRef;
     private FirebaseAuth auth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         connectToFirebase();
+
         Button registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,43 +49,55 @@ public class RegisterActivity extends AppCompatActivity {
                 String lnameEntered = lname.getText().toString();
                 Spinner role = findViewById(R.id.role);
                 String roleEntered = role.getSelectedItem().toString();
-                String errorMessage = "";
+                final String[] errorMessage = {""};
 
                 boolean allDataEntered = !passwordEntered.isEmpty() && !confirmPasswordEntered.isEmpty()
                         && !emailEntered.isEmpty() && !fnameEntered.isEmpty() && !lnameEntered.isEmpty()
                         && !roleEntered.isEmpty();
 
-                if(allDataEntered) {
-                    if(passwordEntered.equals(confirmPasswordEntered)){
-                        auth.createUserWithEmailAndPassword(emailEntered, passwordEntered);
-                        if(auth.getCurrentUser() != null){
-                            DatabaseReference user = firebaseDB.getReference("User/" + auth.getCurrentUser().getUid());
-                            user.child("firstName").setValue(fnameEntered);
-                            user.child("lastName").setValue(lnameEntered);
-                            user.child("role").setValue(roleEntered);
-                            Intent intent = new Intent(RegisterActivity.this, RegisterSuccessActivity.class);
-                            intent.putExtra("name", fnameEntered);
-                            startActivity(intent);
-                        }
-                        else {
-                            errorMessage = "Unknown error occurred. Please try again.";
-                        }
+                if (allDataEntered) {
+                    if (passwordEntered.equals(confirmPasswordEntered)) {
+                        auth.createUserWithEmailAndPassword(emailEntered, passwordEntered)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            FirebaseUser user = auth.getCurrentUser();
+                                            if (user != null) {
+                                                DatabaseReference userRef = firebaseDB.getReference("User/" + user.getUid());
+                                                userRef.child("firstName").setValue(fnameEntered);
+                                                userRef.child("lastName").setValue(lnameEntered);
+                                                userRef.child("role").setValue(roleEntered);
+
+                                                Intent intent = new Intent(RegisterActivity.this, RegisterSuccessActivity.class);
+                                                intent.putExtra("name", fnameEntered);
+                                                startActivity(intent);
+                                            }
+                                        } else {
+                                            try {
+                                                throw Objects.requireNonNull(task.getException());
+                                            } catch (FirebaseAuthUserCollisionException e) {
+                                                errorMessage[0] = "Email is already in use.";
+                                            } catch (Exception e) {
+                                                errorMessage[0] = "Unknown error occurred. Please try again.";
+                                            }
+                                        }
+                                    }
+                                });
+                    } else {
+                        errorMessage[0] = "Passwords do not match!";
                     }
-                    else {
-                        errorMessage = "Passwords do not match!";
-                    }
+                } else {
+                    errorMessage[0] = "Please fill out all fields!";
                 }
-                else {
-                    errorMessage = "Please fill out all fields!";
-                }
-                if(!errorMessage.equals("")){
-                    Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+                if(!errorMessage[0].isEmpty()) {
+                    Toast.makeText(getApplicationContext(), errorMessage[0], Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    private void connectToFirebase(){
+    private void connectToFirebase() {
         firebaseDB = FirebaseDatabase.getInstance();
         firebaseDBRef = firebaseDB.getReference("User");
         auth = FirebaseAuth.getInstance();
