cfd59e6a0a8a5de728ffcde961a5b3af6273f57a
Roshan George Sebastian <rs385930@dal.ca>
2023-10-13 20:01:52 -0300

    Added a back button to the reviewpage

--- app/src/main/java/com/example/myapplication/user_profile_page/user_profile_fragments/ReviewPage.java
+++ app/src/main/java/com/example/myapplication/user_profile_page/user_profile_fragments/ReviewPage.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.user_profile_page.user_profile_fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -8,14 +9,17 @@ import com.example.myapplication.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import androidx.core.content.ContextCompat;
 
 public class ReviewPage extends AppCompatActivity {
 
@@ -23,9 +27,34 @@ public class ReviewPage extends AppCompatActivity {
     private EditText reviewEditText;
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();  // Close the activity and go back to UserProfile
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_review_page);
+
+        Toolbar toolbar = findViewById(R.id.review_page_toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        toolbar.setTitle("Review");
+
+        // Tint the default back arrow icon to white
+        Drawable upArrow = ContextCompat.getDrawable(this, androidx.appcompat.R.drawable.abc_ic_ab_back_material);
+        if (upArrow != null) {
+            upArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+            toolbar.setNavigationIcon(upArrow);
+        }
 
         ratingBar = findViewById(R.id.ratingBar);
         reviewEditText = findViewById(R.id.reviewEditText);
