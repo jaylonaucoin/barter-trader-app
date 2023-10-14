5f25199d07ab9e23b85ce6a51531a89b6906c28a
zhipneg <zh798787@dal.ca>
2023-10-14 13:26:34 +0800

    add 1 function to limit the textinput and 1 unittest

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -62,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(v -> onSubmit());
         cancelButton.setOnClickListener(v -> customerPreference.setText(""));
         saveButton.setOnClickListener(v -> onSave());
+
     }
 
     private void setupSpinners() {
@@ -96,6 +97,51 @@ public class MainActivity extends AppCompatActivity {
         // store the preferences
         Toast.makeText(this, "Preferences: " + type + ", " + color + ", " + customText, Toast.LENGTH_SHORT).show();
     }
+
+
+    }
+
+    void setupSpinners() {
+        // set up type spinner
+        ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(this, R.array.type_options, android.R.layout.simple_spinner_item);
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(typeAdapter);
+
+        // set up color spinner
+        ArrayAdapter<CharSequence> colorAdapter = ArrayAdapter.createFromResource(this, R.array.color_options, android.R.layout.simple_spinner_item);
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorSpinner.setAdapter(colorAdapter);
+    }
+    //set up firebase
+    public void savePreferencesToFirebase(String type, String color, String customText) {
+        Map<String, String> userPreferences = new HashMap<>();
+        userPreferences.put("Type", type);
+        userPreferences.put("Color", color);
+        userPreferences.put("CustomText", customText);
+
+        firebaseDBRef.child("Preference").child("SavedPreference").setValue(userPreferences)
+                .addOnSuccessListener(aVoid -> Toast.makeText(MainActivity.this, "Your information is upload", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "There's some issue cause the failed", Toast.LENGTH_SHORT).show());
+    }
+    //add a method to check the input length not too long
+    public static boolean checkStringLength(String input, int character) {
+        return input.length() > character;
+    }
+
+    public void onSubmit() {
+        String type = typeSpinner.getSelectedItem().toString();
+        String color = colorSpinner.getSelectedItem().toString();
+        String customText = customerPreference.getText().toString();
+        if (checkStringLength(customText, 50)) {
+            Toast.makeText(this, "No more than 50 characters", Toast.LENGTH_SHORT).show();
+            return; // stop
+        }
+        savePreferencesToFirebase(type, color, customText);
+
+        // store the preferences
+        Toast.makeText(this, "Preferences: " + type + ", " + color + ", " + customText, Toast.LENGTH_SHORT).show();
+    }
+
 
     public void onSave() {
         // saved message
