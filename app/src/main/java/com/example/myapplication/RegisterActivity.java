b235647f7d0195d987320a996d14ffb1fa702eca
jaucoin <jy349203@dal.ca>
2023-10-13 22:02:42 -0300

    Added comments to RegisterActivity and RegisterSuccessActivity. Also set up MainActivity and associated files for future merge with login branch.

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -24,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
+    // Firebase references
     private FirebaseDatabase firebaseDB;
     private DatabaseReference firebaseDBRef;
     private FirebaseAuth auth;
@@ -32,12 +33,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Connect to Firebase DB
         connectToFirebase();
 
+        // Find and configure the register button
         Button registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Get user input data
                 EditText password = findViewById(R.id.password);
                 String passwordEntered = password.getText().toString();
                 EditText confirmPassword = findViewById(R.id.confirmPassword);
@@ -50,33 +55,42 @@ public class RegisterActivity extends AppCompatActivity {
                 String lnameEntered = lname.getText().toString();
                 Spinner role = findViewById(R.id.role);
                 String roleEntered = role.getSelectedItem().toString();
-                final String[] errorMessage = {""};
+                final String[] errorMessage = {""}; // Initialize an error message
 
+                // Check if all required fields are entered
                 boolean allDataEntered = !passwordEntered.isEmpty() && !confirmPasswordEntered.isEmpty()
                         && !emailEntered.isEmpty() && !fnameEntered.isEmpty() && !lnameEntered.isEmpty()
                         && !roleEntered.isEmpty();
 
                 if (allDataEntered) {
+                    // Validate email
                     if(Patterns.EMAIL_ADDRESS.matcher(emailEntered).matches()) {
+                        // Check if passwords match
                         if(passwordEntered.equals(confirmPasswordEntered)) {
+                            // Check password length according to Firebase rules (6 chars long)
                             if(passwordEntered.length() >= 6) {
+                                // Create a user using Firebase authentication
                                 auth.createUserWithEmailAndPassword(emailEntered, passwordEntered)
                                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                             @Override
                                             public void onComplete(@NonNull Task<AuthResult> task) {
                                                 if (task.isSuccessful()) {
+                                                    // Registration is successful
                                                     FirebaseUser user = auth.getCurrentUser();
                                                     if (user != null) {
+                                                        // Update user information in the Realtime Database
                                                         DatabaseReference userRef = firebaseDB.getReference("User/" + user.getUid());
                                                         userRef.child("firstName").setValue(fnameEntered);
                                                         userRef.child("lastName").setValue(lnameEntered);
                                                         userRef.child("role").setValue(roleEntered);
 
+                                                        // Proceed to the success page
                                                         Intent intent = new Intent(RegisterActivity.this, RegisterSuccessActivity.class);
                                                         intent.putExtra("name", fnameEntered);
                                                         startActivity(intent);
                                                     }
                                                 } else {
+                                                    // Handle different error cases starting here
                                                     try {
                                                         throw Objects.requireNonNull(task.getException());
                                                     } catch (FirebaseAuthUserCollisionException e) {
@@ -99,7 +113,9 @@ public class RegisterActivity extends AppCompatActivity {
                 } else {
                     errorMessage[0] = "Please fill out all fields!";
                 }
-                if(!errorMessage[0].isEmpty()) {
+
+                // Display the error message if any
+                if (!errorMessage[0].isEmpty()) {
                     Toast.makeText(getApplicationContext(), errorMessage[0], Toast.LENGTH_SHORT).show();
                 }
             }
@@ -107,6 +123,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void connectToFirebase() {
+        // Initialize Firebase instances
         firebaseDB = FirebaseDatabase.getInstance();
         firebaseDBRef = firebaseDB.getReference("User");
         auth = FirebaseAuth.getInstance();
--- app/src/main/java/com/example/myapplication/RegisterSuccessActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterSuccessActivity.java
@@ -22,14 +22,21 @@ public class RegisterSuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_success);
+
+        // Get the user's first name from the previous activity
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
+
+        // Find the welcome message TextView and set it with user's name
         TextView welcomeMessage = findViewById(R.id.welcomeMessage);
         welcomeMessage.setText("Welcome " + name + "!");
+
+        // Initialize Firebase components
         connectToFirebase();
     }
 
     private void connectToFirebase(){
+        // Initialize Firebase instances
         firebaseDB = FirebaseDatabase.getInstance();
         firebaseDBRef = firebaseDB.getReference("User");
         auth = FirebaseAuth.getInstance();
