3407d5d4d608f6825503dbe0d201bd3c6285e71d
zhipneg <zh798787@dal.ca>
2023-11-04 02:40:34 +0800

    2espresso and 2unit test create

---
+++ app/src/androidTest/java/com/example/myapplication/ValueSerEspressoTest.java
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Context;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ValueSerEspressoTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.myapplication", appContext.getPackageName());
+    }
+
+    //work
+    public ActivityScenario<ValuationService> scenario;
+
+    @Before
+    public void setup() {
+        scenario = ActivityScenario.launch(ValuationService.class);
+        scenario.onActivity(activity -> {
+        });
+    }
+
+
+    @Test
+    public void testTotalPriceText() {
+        onView(withId(R.id.tvTotalValue)).check(matches(isDisplayed()));
+        onView(withId(R.id.Buy)).check(matches(isDisplayed()));
+        onView(withId(R.id.Sell)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSellButtonUpdates() {
+        //check input the name and value
+        onView(withId(R.id.etItemName)).perform(typeText("Apple"));
+        onView(withId(R.id.etItemValue)).perform(typeText("10.0"));
+
+        //click sell button
+        onView(withId(R.id.Sell)).perform(click());
+
+        //click yes in dialog
+        onView(withText("Yes")).perform(click());
+
+        //check the update
+        onView(withId(R.id.tvTotalValue)).check(matches(withText("Your total value: 10.0")));
+    }
+
+
+    //end
+}
---
+++ app/src/test/java/com/example/myapplication/ValueSerUnitTest.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ValueSerUnitTest {
+    private ExchangeCalculate calculator;
+    private ArrayList<String> items;
+
+    @Before
+    public void setUp() {
+        items = new ArrayList<>();
+        calculator = new ExchangeCalculate(items);
+    }
+
+    @Test
+    public void testSellItem() {
+        calculator.sellItem("Apple", 10.0);
+        assertEquals(10.0, calculator.getTotalValue(), 0.001);
+        assertEquals(1, items.size());
+        assertEquals("Apple - 10.0", items.get(0));
+    }
+    @Test
+    public void testBuyItem() {
+        calculator.sellItem("Apple", 10.0);
+        calculator.buyItem(0);
+        assertEquals(0.0, calculator.getTotalValue(), 0.001);
+        assertEquals(0, items.size());
+    }
+
+}
