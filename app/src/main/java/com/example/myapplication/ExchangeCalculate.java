c817401d267fd49aa746994ecee8652f9df5aa95
zhipneg <zh798787@dal.ca>
2023-11-04 01:58:17 +0800

    button function and extend list

---
+++ app/src/main/java/com/example/myapplication/ExchangeCalculate.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class ExchangeCalculate {
+    //set the start value to 0
+    private double totalValue = 0;
+    //build array
+    private ArrayList<String> items;
+
+    public ExchangeCalculate(ArrayList<String> initialItems) {
+        this.items = initialItems;
+    }
+
+    public void sellItem(String itemName, double itemValue) {
+        //calculate the value
+        totalValue += itemValue;
+        items.add(itemName + " - " + itemValue);
+    }
+
+    public void buyItem(int position) {
+        if (position != -1 && position < items.size()) {
+            String selectedItem = items.get(position);
+            String[] parts = selectedItem.split(" - ");
+            double itemValue = Double.parseDouble(parts[1]);
+            //calculate the value and remove from original position
+            totalValue -= itemValue;
+            items.remove(position);
+        }
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+}
--- app/src/main/java/com/example/myapplication/ValuationService.java
+++ app/src/main/java/com/example/myapplication/ValuationService.java
@@ -1,17 +1,31 @@
 package com.example.myapplication;
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 public class ValuationService {
 =======
+=======
+import android.app.AlertDialog;
+>>>>>>> Stashed changes
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+<<<<<<< Updated upstream
 
 import androidx.appcompat.app.AppCompatActivity;
 
+=======
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+>>>>>>> Stashed changes
 import java.util.ArrayList;
 
 public class ValuationService extends AppCompatActivity {
@@ -21,6 +35,7 @@ public class ValuationService extends AppCompatActivity {
     private ListView listView;
     private ArrayList<String> items;
     private ArrayAdapter<String> adapter;
+<<<<<<< Updated upstream
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,3 +60,80 @@ public class ValuationService extends AppCompatActivity {
 
 >>>>>>> Stashed changes
 }
+=======
+    private ExchangeCalculate calculate;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.valuation_service);
+
+            tvTotalValue = findViewById(R.id.tvTotalValue);
+            etItemName = findViewById(R.id.etItemName);
+            etItemValue = findViewById(R.id.etItemValue);
+            Sell = findViewById(R.id.Sell);
+            Buy = findViewById(R.id.Buy);
+            listView = findViewById(R.id.listView);
+
+            //build a new list to save the stuff
+            items = new ArrayList<>();
+            adapter = new ArrayAdapter<>(this, R.layout.list_extend, items);
+            listView.setAdapter(adapter);
+            calculate = new ExchangeCalculate(items);
+            //behavior on sell button
+            Sell.setOnClickListener(v -> {
+                String itemName = etItemName.getText().toString();
+                double itemValue;
+
+                // avoid nothing input
+                try {
+                    itemValue = Double.parseDouble(etItemValue.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(this, "Please enter the price.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //have a dialog to make sure for user
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Are you sure to sell?");
+                builder.setMessage("You want sell " + itemName + " and get " + itemValue + " dollars in your total value ?");
+                builder.setPositiveButton("Yes", (dialog, which) -> {
+                    //if chose yes
+                    calculate.sellItem(itemName, itemValue);
+                    tvTotalValue.setText("Your total value: " + calculate.getTotalValue());
+                    adapter.notifyDataSetChanged();
+                    Toast.makeText(ValuationService.this, "Great you sell " + itemName + " !", Toast.LENGTH_SHORT).show();
+
+
+                });
+                //if chose no
+                builder.setNegativeButton("Cancel", null);
+                builder.create().show();
+            });
+            //behavior on buy button
+            Buy.setOnClickListener(v -> {
+                int position = listView.getCheckedItemPosition();
+                if (position != ListView.INVALID_POSITION) {
+                    String selectedItem = items.get(position);
+                    String[] parts = selectedItem.split(" - ");
+                    String itemName = parts[0];
+                    //same as sell button
+                    new AlertDialog.Builder(ValuationService.this)
+                            .setTitle("Confirm to buy")
+                            .setMessage("Are you sure you want to buy " + itemName + " ?")
+                            .setPositiveButton("Yes", (dialog, which) -> {
+                                calculate.buyItem(position);
+                                tvTotalValue.setText("Your total value: " + calculate.getTotalValue());
+                                adapter.notifyDataSetChanged();
+                                Toast.makeText(ValuationService.this, "Great! you got " + itemName + " !", Toast.LENGTH_SHORT).show();
+
+
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                }
+            });
+        }
+}
+
+>>>>>>> Stashed changes
