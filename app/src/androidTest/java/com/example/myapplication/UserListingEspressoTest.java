f75f709a3880fd7d3c8266ced23df82c6f497f1c
Connor <cn560967@dal.ca>
2023-11-04 14:34:52 -0300

    finished user listing page espresso tests

--- app/src/androidTest/java/com/example/myapplication/UserListingEspressoTest.java
+++ app/src/androidTest/java/com/example/myapplication/UserListingEspressoTest.java
@@ -1,45 +1,20 @@
 package com.example.myapplication;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-import android.content.Context;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.w3c.dom.Text;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.*;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 @RunWith(AndroidJUnit4.class)
-public class UserListingTest {
+public class UserListingEspressoTest {
     public ActivityScenario<LoginActivity> scenario;
     FirebaseAuth auth;
 
@@ -50,7 +25,7 @@ public class UserListingTest {
     }
 
     @Test
-    public void testListingsDisplayed() {
+    public void login() {
         // Enter valid email and password, then click the "Login" button
         Espresso.onView(ViewMatchers.withId(R.id.email)).perform(ViewActions.typeText("roshanplayzmc@gmail.com"));
         Espresso.onView(ViewMatchers.withId(R.id.password)).perform(ViewActions.typeText("admin123"));
@@ -63,7 +38,35 @@ public class UserListingTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        // Verify that the SuccessActivity is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.instructions)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+    @Test
+    public void testListingsDisplayed() {
+        // Enter valid email and password, then click the "Login" button
+        login();
+        Espresso.onView(ViewMatchers.withId(R.id.userListing)).perform(ViewActions.click());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Espresso.onView(ViewMatchers.withId(R.id.listingsListView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void returnToSuccessButton() {
+        login();
         Espresso.onView(ViewMatchers.withId(R.id.userListing)).perform(ViewActions.click());
         Espresso.onView(ViewMatchers.withId(R.id.listingsListView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        Espresso.onView(ViewMatchers.withId(R.id.returnSuccess)).perform(ViewActions.click());
+        // Introduce a delay wait for SuccessActivity
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Verify that the SuccessActivity is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.instructions)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
