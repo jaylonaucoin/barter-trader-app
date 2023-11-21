a9893f2957f74c8759dc360bcd9a5f4d8ff2a26c
Connor <cn560967@dal.ca>
2023-11-21 14:01:38 -0400

    created espresso test to login and access post page, now need to implement espresso test for posting with categories

---
+++ app/src/androidTest/java/com/example/myapplication/CategoryPostEspressoTest.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class CategoryPostEspressoTest {
+    public ActivityScenario<LoginActivity> scenario;
+    FirebaseAuth auth;
+
+    @Before
+    public void setup() {
+        scenario = ActivityScenario.launch(LoginActivity.class);
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Before
+    public void loginUser() {
+        onView(withId(R.id.email)).perform(typeText("test@test.com"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("admintest123"), closeSoftKeyboard());
+        onView(withId(R.id.loginButton)).perform(click());
+
+        // Add delay or IdlingResource to ensure login process completes before subsequent tests
+        try {
+            Thread.sleep(2000);  // wait for 2 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void clickPost() {
+        onView(withId(R.id.postGoodsButton)).perform(click());
+
+        // Add delay or IdlingResource to ensure login process completes before subsequent tests
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Espresso.onView(ViewMatchers.withId(R.id.condition)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+}
