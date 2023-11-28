b32d94bd7f7363cc365bf99e80cefacc875577f3
Connor <cn560967@dal.ca>
2023-11-28 19:47:29 -0400

    Mockito test for selecting specific category, fails as code is not complete

--- app/src/androidTest/java/com/example/myapplication/CategoryPostEspressoTest.java
+++ app/src/androidTest/java/com/example/myapplication/CategoryPostEspressoTest.java
@@ -1,22 +1,25 @@
 package com.example.myapplication;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.hamcrest.Matchers;
+import com.google.firebase.auth.FirebaseAuth;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -72,4 +75,27 @@ public class CategoryPostEspressoTest {
                 .check(ViewAssertions.matches(ViewMatchers.withText("Furniture")));
     }
 
+    @Test
+    public void selectCategoryFurniture() {
+        // Mock the behavior for category selection (assuming RecyclerView with category items)
+        YourRecyclerViewAdapter mockAdapter = mock(YourRecyclerViewAdapter.class);
+        when(mockAdapter.getItemCount()).thenReturn(3); // Example count of categories
+        when(mockAdapter.getItemViewType(0)).thenReturn(0); // Assuming view type for categories
+        when(mockAdapter.getItemViewType(1)).thenReturn(0);
+        when(mockAdapter.getItemViewType(2)).thenReturn(0);
+
+        // Mock category names in the adapter
+        when(mockAdapter.getItemText(0)).thenReturn("Electronics");
+        when(mockAdapter.getItemText(1)).thenReturn("Clothing");
+        when(mockAdapter.getItemText(2)).thenReturn("Furniture");
+
+        // Perform actions to select a specific category (for example, Furniture)
+        onView(withId(R.id.recyclerView)) // Replace with your RecyclerView ID
+                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click())); // Click on Furniture
+
+        // Check if the selected category is displayed or not
+        Espresso.onView(ViewMatchers.withId(R.id.selectedCategoryTextView)) // Replace with your TextView ID
+                .check(matches(withText("Furniture")));
+    }
+}
 }
