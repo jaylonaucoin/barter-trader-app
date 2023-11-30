b474da39a8b1541efc0b972f4308c2c876b0b327
Connor <cn560967@dal.ca>
2023-11-30 19:49:39 -0400

    Test Cases! added more tests for category posts

--- app/src/androidTest/java/com/example/myapplication/CategoryPostEspressoTest.java
+++ app/src/androidTest/java/com/example/myapplication/CategoryPostEspressoTest.java
@@ -7,16 +7,16 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -65,37 +65,23 @@ public class CategoryPostEspressoTest {
 
     @Test
     public void selectCategoryClothing() {
-
-
         // Perform actions to select a different category (for example, Clothing)
         onView(withId(R.id.category)).perform(click());
+        onView(withText("Clothing & Accessories")).perform(click());
 
-        // Check if the selected category is displayed or not
+        // Check if the selected category is displayed in the spinner
         Espresso.onView(ViewMatchers.withId(R.id.category))
-                .check(ViewAssertions.matches(ViewMatchers.withText("Furniture")));
+                .check(matches(withSpinnerText("Clothing & Accessories")));
     }
 
     @Test
-    public void selectCategoryFurniture() {
-        // Mock the behavior for category selection (assuming RecyclerView with category items)
-        YourRecyclerViewAdapter mockAdapter = mock(YourRecyclerViewAdapter.class);
-        when(mockAdapter.getItemCount()).thenReturn(3); // Example count of categories
-        when(mockAdapter.getItemViewType(0)).thenReturn(0); // Assuming view type for categories
-        when(mockAdapter.getItemViewType(1)).thenReturn(0);
-        when(mockAdapter.getItemViewType(2)).thenReturn(0);
+    public void selectCategoryElectronics() {
+        // Perform actions to select Electronics category
+        onView(withId(R.id.category)).perform(click());
+        onView(withText("Electronics")).perform(click());
 
-        // Mock category names in the adapter
-        when(mockAdapter.getItemText(0)).thenReturn("Electronics");
-        when(mockAdapter.getItemText(1)).thenReturn("Clothing");
-        when(mockAdapter.getItemText(2)).thenReturn("Furniture");
-
-        // Perform actions to select a specific category (for example, Furniture)
-        onView(withId(R.id.recyclerView)) // Replace with your RecyclerView ID
-                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click())); // Click on Furniture
-
-        // Check if the selected category is displayed or not
-        Espresso.onView(ViewMatchers.withId(R.id.selectedCategoryTextView)) // Replace with your TextView ID
-                .check(matches(withText("Furniture")));
+        // Check if the selected category is displayed in the spinner
+        Espresso.onView(ViewMatchers.withId(R.id.category))
+                .check(matches(withSpinnerText("Electronics")));
     }
-}
 }
