bc7f3c32383ab33738065de2041c27df2efb993e
Roshan George Sebastian <rs385930@dal.ca>
2023-12-03 14:36:25 -0400

    Fixed SearchEspressoTest

--- app/src/androidTest/java/com/example/myapplication/SearchEspressoTest.java
+++ app/src/androidTest/java/com/example/myapplication/SearchEspressoTest.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -11,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
@@ -24,17 +26,31 @@ import static org.hamcrest.core.StringContains.containsString;
 @LargeTest
 public class SearchEspressoTest {
 
-    private ActivityScenario<SearchActivity> searchScenario;
-
     @Before
     public void setup() {
-        searchScenario = ActivityScenario.launch(SearchActivity.class);
+        ActivityScenario<LoginActivity> searchScenario = ActivityScenario.launch(LoginActivity.class);
+
+        // Enter valid email and password, then click the "Login" button
+        Espresso.onView(ViewMatchers.withId(R.id.email)).perform(ViewActions.typeText("ros@gmail.com"));
+        Espresso.onView(ViewMatchers.withId(R.id.password)).perform(ViewActions.typeText("admin123"));
+        Espresso.closeSoftKeyboard(); // Close the keyboard
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click());
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.navigation_search))
+                .perform(click());
     }
 
     @Test
     public void testSearchByName() {
         // When the user enters a name (e.g., "Laptop") and initiates the search
         onView(withId(R.id.nameEditText)).perform(ViewActions.typeText("Laptop"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.searchButton)).perform(ViewActions.click());
 
         // Then verify that the search results activity is displayed
@@ -49,6 +65,7 @@ public class SearchEspressoTest {
     public void testSearchByExchangePreference() {
         // When the user enters an exchange preference (e.g., "TV") and initiates the search
         onView(withId(R.id.exchangePreferenceEditText)).perform(ViewActions.typeText("TV"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.searchButton)).perform(ViewActions.click());
 
         // Then verify that the search results activity is displayed
@@ -64,6 +81,7 @@ public class SearchEspressoTest {
         // When the user enters a condition (e.g., "Used (Very Good)") and initiates the search
         onView(withId(R.id.conditionSpinner)).perform(ViewActions.click());
         onView(ViewMatchers.withText("Used (Very Good)")).perform(ViewActions.click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.searchButton)).perform(ViewActions.click());
 
         // Then verify that the search results activity is displayed
@@ -78,9 +96,11 @@ public class SearchEspressoTest {
     public void testSearchWithMultipleCriteria() {
         // When the user sets various search criteria (e.g., name, condition, exchange preference) and initiates the search
         onView(withId(R.id.nameEditText)).perform(ViewActions.typeText("Laptop"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.conditionSpinner)).perform(ViewActions.click());
         onView(ViewMatchers.withText("Used (Very Good)")).perform(ViewActions.click());
         onView(withId(R.id.exchangePreferenceEditText)).perform(ViewActions.typeText("TV"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.searchButton)).perform(ViewActions.click());
 
         // Then verify that the search results activity is displayed
@@ -111,6 +131,7 @@ public class SearchEspressoTest {
     public void testNoMatchingResults() {
         // When the user enters a name (e.g., "Laptop") and initiates the search
         onView(withId(R.id.nameEditText)).perform(ViewActions.typeText("abcdefghijklmnop"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.searchButton)).perform(ViewActions.click());
 
         // Then verify that an error message is displayed indicating that there are no results
